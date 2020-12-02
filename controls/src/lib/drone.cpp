345047d2780222fc7041fa03d8980471d9782646
James T <58379166+jtfa22@users.noreply.github.com>
2020-12-01 21:18:04 -0800

    add basic telemetry implementation
    for values needed in DroneStatus

--- controls/src/lib/drone.cpp
+++ controls/src/lib/drone.cpp
@@ -4,11 +4,47 @@
 Drone::Drone()
 {
 	//TODO: default constructor?
+    std::cout<<"\n\n\nbonkers.\n\n\n"<<std::endl;
 }
 
-Drone::Drone(std::string connection_url)
+Drone::Drone(std::string drone_id, std::string connection_url)
 {
-	system = connect_to_pixhawk(connection_url);
+    system = connect_to_pixhawk(connection_url);
+    this.drone_id = drone_id;
+    init_telem();
+    drone_state = STANDBY;
+
+    drone_status.drone_id = this.drone_id;
+    drone_status.drone_state = drone_state;
+    drone_status.docking_slot = docking_slot;
+}
+
+void Drone::init_telem()
+{
+    telem = std::make_shared<mavsdk::Telemetry>(system);
+
+    telemetry->subscribe_position([&gps_position](mavsdk::Telemetry::Position position) {
+        gps_position = position;
+    }); 
+
+    telemetry->subscribe_attitude_quaternion([&att_quaternion](mavsdk::Telemetry::AttitudeQuaternionCallback quaternion ){
+        att_quaternion = quaternion;
+    });
+
+    telemetry->subscribe_battery([&batt](mavsdk::Telemetry::BatteryCallback battery){
+        batt = battery;
+    });
+
+    // more telemetry
+}
+
+void update_drone_status()
+{
+    drone_status.drone_state = drone_state;
+    drone_status.docking_slot = docking_slot;
+    drone_status.gps_position = gps_position;
+    drone_status.yaw = att_quaternion.z; //TODO: verify 
+    drone_status.battery_percent = batt.remaining_percent;
 }
 
 void Drone::arm_drone() // for drones in STANDBY / DOCKED_FOLLOWER
--- controls/src/lib/drone.hpp
+++ controls/src/lib/drone.hpp
@@ -15,26 +15,48 @@ enum DroneState {
     NEEDS_SERVICE
 };
 
+// same types from mavsdk so it can directly pass in data from px4
+struct GpsPosition { double latitude_deg; double longitude_deg; float absolute_altitude_m; float relative_altitude_m; }; //https://mavsdk.mavlink.io/develop/en/api_reference/structmavsdk_1_1_telemetry_1_1_position.html
+struct AttQuatternion { float w; float x; float y; float z; }; //https://mavsdk.mavlink.io/develop/en/api_reference/structmavsdk_1_1_telemetry_1_1_quaternion.html
+struct Battery { float voltage_v; float remaining_percent; }; //https://mavsdk.mavlink.io/develop/en/api_reference/structmavsdk_1_1_telemetry_1_1_battery.html
+
+// reference values (copy of values within Drone)
 struct DroneStatus {
-    std::string id;
-    DroneState state;
+    std::string drone_id;
+    DroneState drone_state;
     uint8_t docking_slot;
-    // add telemetry
+
+    float battery_percent; 
+    GpsPosition gps_position;
+    float yaw;
 };
 
 class Drone
 {
 	public:
     Drone();
-    Drone(std::string connection_url);
+    Drone(std::string drone_id, std::string connection_url);
 
 	// AVIATA 
-	std::string drone_name;
+	//std::string drone_name;
+    std::string drone_id;
     DroneState drone_state;
+    DroneStatus drone_status;
+    uint8_t docking_slot = -1;
 
 	//MAVSDK
 	std::string connection_url;
 	std::shared_ptr<mavsdk::System> system;
+    std::shared_ptr<mavsdk::Telemetry> telem;
+
+    //TELEMETRY
+    GpsPosition gps_position;
+    AttQuatternion att_quaternion;
+    Battery batt;
+
+    void init_telem();
+
+    void update_drone_status(); // call before sending data
 
     void arm_drone(); // for drones in STANDBY / DOCKED_FOLLOWER
     
