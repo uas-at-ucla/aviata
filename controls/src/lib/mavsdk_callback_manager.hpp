65e269d693ceabba906f130e5ef6251ab7b2db09
Ryan Nemiroff <ryguyn@gmail.com>
2020-12-30 14:31:30 -0800

    Update MavsdkCallbackManager

--- controls/src/lib/mavsdk_callback_manager.hpp
+++ controls/src/lib/mavsdk_callback_manager.hpp
@@ -4,6 +4,8 @@
 #include <functional>
 #include <map>
 #include <mutex>
+#include <memory>
+#include <list>
 
 class MavsdkCallbackManager
 {
@@ -13,25 +15,27 @@ public:
         uint32_t this_id = id;
         subscribe([this, this_id, user_callback](T arg) {
             std::lock_guard<std::mutex> guard(mutex);
-            mavsdk_callback_queue[this_id] = [user_callback, arg]() {
+            mavsdk_callback_queue[this_id] = std::make_unique<std::function<void()>>([user_callback, arg]() {
                 user_callback(arg);
-            };
+            });
         });
         id++;
     }
 
     void call_queued_mavsdk_callbacks() {
-        std::lock_guard<std::mutex> guard(mutex);
-        auto iterator = mavsdk_callback_queue.begin();
-        while (iterator != mavsdk_callback_queue.end()) {
-            iterator->second();
-            iterator = mavsdk_callback_queue.erase(iterator);
+        std::map<uint32_t, std::unique_ptr<std::function<void()>>> queued_callbacks;
+        {
+            std::lock_guard<std::mutex> guard(mutex);
+            queued_callbacks.merge(mavsdk_callback_queue);
+        }
+        for (const auto& [id, callback] : queued_callbacks) {
+            (*callback)();
         }
     }
 
 private:
     static uint32_t id;
-    std::map<uint32_t, std::function<void()>> mavsdk_callback_queue;
+    std::map<uint32_t, std::unique_ptr<std::function<void()>>> mavsdk_callback_queue;
     std::mutex mutex;
 };
 
