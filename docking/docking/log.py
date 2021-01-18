b84aeb9901c386eb15ecf1bde9cfae400b9a0cb6
osboxes.org <chirag.singh3489@gmail.com>
2021-01-18 00:17:28 -0500

    Typo fix

--- docking/docking/log.py
+++ docking/docking/log.py
@@ -11,7 +11,7 @@ class Log:
 
     def write(self,droneX,droneY,droneAlt,droneYaw,tagsDetected,successful_frames,x_err,y_err,alt_err,yaw_err):
         file=open(self.filename,'a')
-        currLog="Drone X: "+str(format(droneX,'0.2f'))+", Drone Y: "+str(format(droneY,'0.2f'))+", Drone Alt: "+str(format(droneAlt,'0.2f'))+", Drone Yaw: "+str(format(droneYaw,'0.2f'))+", x_err: "+str(format(x_err,'0.2f'))+ ", y_err: "+str(format(y_err,'0.2f'))+ ", alt_err: "+str(format(alt_err,'0.2f'))+ ", x_err: "+str(format(yaw_err,'0.2f'))+", Tags Detected: "+str(tagsDetected)+", Successful Frames: "+str(successful_frames)+"\n"
+        currLog="Drone X: "+str(format(droneX,'0.2f'))+", Drone Y: "+str(format(droneY,'0.2f'))+", Drone Alt: "+str(format(droneAlt,'0.2f'))+", Drone Yaw: "+str(format(droneYaw,'0.2f'))+", x_err: "+str(format(x_err,'0.2f'))+ ", y_err: "+str(format(y_err,'0.2f'))+ ", alt_err: "+str(format(alt_err,'0.2f'))+ ", rot_err: "+str(format(yaw_err,'0.2f'))+", Tags Detected: "+str(tagsDetected)+", Successful Frames: "+str(successful_frames)+"\n"
         file.write(currLog)
         file.close()
 
