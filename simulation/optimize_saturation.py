37b60a7a5cbc8cd2d775ed67bf894358f6556816
Ryan Nemiroff <ryguyn@gmail.com>
2021-03-20 19:01:20 -0700

    Better version of saturation minimizer

--- simulation/optimize_saturation.py
+++ simulation/optimize_saturation.py
@@ -1,100 +1,53 @@
-# Based on https://math.stackexchange.com/questions/2659023/minimal-ell-infty-solution-of-underdetermined-system-of-linear-equations
+# Inspired by https://math.stackexchange.com/questions/2659023/minimal-ell-infty-solution-of-underdetermined-system-of-linear-equations
 
 import numpy as np
 import cvxpy as cvx
 
-# TODO this needs to do more than simply minimize infinity norm, because:
-#    1) thrust cannot be negative
-#    2) motors not used much for thrusting cannot be used much for attitude
-#    An idea: optimize thrust first, then optimize for attitude saturation given 50% thrust
-
-def min_sat_above_0_sol(A, i, val, ignore_z_thrust=False, B5=None):
-    n = A.shape[0] # number of rows of A
-    m = A.shape[1] # number of columns of A
-
-    b = np.zeros(n)
-    b[i] = val
+def maximize_setpoint(A, i, b, lower_bounds, upper_bounds, negative=False):
+    # n = A.shape[0] # number of rows of A (setpoints)
+    m = A.shape[1] # number of columns of A (rotors)
 
     # check that system has solution
-    if (np.linalg.matrix_rank(np.column_stack((A,b))) - np.linalg.matrix_rank(A)):
-        return np.zeros(m)
+    # if (np.linalg.matrix_rank(np.column_stack((A,b))) - np.linalg.matrix_rank(A)):
+    #     return np.zeros(m)
 
-    # matrix costraints for equality Ax = b
-    A_eq = A
-    b_eq = b
-    n_eq = n
-    if ignore_z_thrust:
-        A_eq = A_eq[:-1] # last row is z thrust
-        b_eq = b[:-1]
-        n_eq = n-1
-    A_eq = np.column_stack((A_eq, np.zeros(n_eq)))
+    # constraints for equality Ax = b, excluding the value to maximize
+    A_eq = np.delete(A, i, 0)
+    b_eq = np.delete(b, i, 0)
 
-    # matrix constraint for |x|<s
-        # M_1 = np.column_stack((np.diag(np.ones(m)),  np.ones(m)))
-        # b_1 = np.zeros(m)
-
-        # M_2 = np.column_stack(( - np.diag(np.ones(m)),  np.ones(m)))
-        # b_2 = np.zeros(m)
-        # # contraint s > 0
-        # M_3 = np.zeros(M_2.shape[1])
-        # M_3[-1] = 1
-        # b_3 = [0.]
-
-        # A_ub = np.row_stack((M_1, M_2, M_3))
-        # b_ub = np.concatenate((b_1, b_2, b_3))
-
-    # Don't utilize rotors that don't contribute to rotation
-    A_z = []
-    b_z = []
-    irrelevant_rotors = []
-    if ignore_z_thrust:
-        for j in range(m):
-            if abs(A[i,j]) < 1e-6:
-                irrelevant_rotors.append(j)
-                sel_x_j = np.zeros(m)
-                sel_x_j[j] = 1
-                A_z.append(np.column_stack(([sel_x_j - A[5]*B5[j]], [[0]])))
-                b_z.append([0])
-    if len(A_z) > 0:
-        A_eq = np.row_stack([A_eq] + A_z)
-        b_eq = np.concatenate([b_eq] + b_z)
-
-    # matrix constraint for x<s and x>0
-    M_1 = np.column_stack(( - np.diag(np.ones(m)),  np.ones(m)))
-    b_1 = np.zeros(m)
-    M_1 = np.delete(M_1, irrelevant_rotors, 0)
-    b_1 = np.delete(b_1, irrelevant_rotors, 0)
-    # contraint x > 0
-    M_2 = np.column_stack((np.diag(np.ones(m)),  np.zeros(m)))
-    b_2 = np.zeros(m)
-    M_2 = np.delete(M_2, irrelevant_rotors, 0)
-    b_2 = np.delete(b_2, irrelevant_rotors, 0)
-
-    A_ub = np.row_stack((M_1, M_2))
-    b_ub = np.concatenate((b_1, b_2))
-
-    # define target
-    c = np.append(np.zeros(m), [1.])
+    # multiply A[i] by -1 if value to maximize is negative (want to maximize magnitude)
+    A_i_obj = A[i] * (-1 if negative else 1)
 
     # Construct the problem.
-    x = cvx.Variable(c.shape[0])
-    objective = cvx.Minimize(c @ x)
-    constraints = [A_eq@x == b_eq, A_ub@x >= b_ub ]
+    x = cvx.Variable(m)
+    objective = cvx.Maximize(A_i_obj @ x)
+    constraints = [A_eq@x == b_eq, x >= lower_bounds, x <= upper_bounds ]
     prob = cvx.Problem(objective, constraints)
 
     # The optimal objective is returned by prob.solve().
     result = prob.solve()
     # The optimal value for x is stored in x.value.
-    return x.value[:-1]
+    return x.value
 
 def optimal_inverse(A):
-    B = np.zeros([A.shape[1], A.shape[0]])
-    B[:,5] = -min_sat_above_0_sol(A, 5, -1)
+    n = A.shape[0] # number of rows of A (setpoints)
+    m = A.shape[1] # number of columns of A (rotors)
+    B = np.zeros([m, n])
+    thrust_sol = maximize_setpoint(A, 5, np.zeros(n), np.zeros(m), np.ones(m), negative=True)
+    max_thrust = A[5] @ thrust_sol
+    B[:,5] = thrust_sol / max_thrust
+
+    # constrain torque control allocation at hover thrust
+    hover_thrust = max_thrust / 2 # assume hover thrust is 50% of max thrust
+    hover_thrust_vals = B[:,5] * hover_thrust
+    inverse_hover_thrust_vals = 1 - hover_thrust_vals
+    torque_contraints = np.minimum(hover_thrust_vals, inverse_hover_thrust_vals)
+
     for i in [0,1,2]:
-        torque_sol = min_sat_above_0_sol(A, i, 1, ignore_z_thrust=True, B5=B[:,5])
-        thrust = A[5] @ torque_sol
-        B[:,i] = torque_sol - thrust*B[:,5]
-        assert(abs(A[5] @ B[:,i]) < 1e-6)
+        torque_sol = maximize_setpoint(A, i, np.zeros(n), -torque_contraints, torque_contraints)
+        max_torque = A[i] @ torque_sol
+        B[:,i] = torque_sol / max_torque
+    
     return B
 
 
