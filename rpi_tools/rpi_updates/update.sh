f4a27197b91b35f554534534621c7642e8b58c27
atpngo <andyngo2021@gmail.com>
2022-02-24 18:38:16 -0800

    updated the script to match ./aviata_drone arguments (hopefully lol)

--- rpi_tools/rpi_updates/update.sh
+++ rpi_tools/rpi_updates/update.sh
@@ -6,32 +6,41 @@ use_guide () {
     echo "  -s: state (standby, follower, leader)"
     echo "  -d: docking slot"
     echo "  -c: connection url"
-    echo "  -n: number of drones"
+    echo "  -f: airframe in use ('4', '4_alt', or '2_sim')"
     echo "  -m: max-missing"
     echo "  -w: network name the Rpi connects to"
     echo "  -p: password to the network"
 }
 '''
+
+INPUTARGS=""
+
 while getopts i:s:d:c:n:m:w:p: flag
 do
     case "${flag}" in
-        i) id=${OPTARG};;
-        s) state="${OPTARG}"
-            if [ "$state" == leader ] || [ "$state" == follower ] || [ "$state" == standby ]; then
-                echo "valid input"
-            else
-                use_guide
-                exit 0
-            fi
+        i) id=${OPTARG}
+            INPUTARGS+="--id $id "
         ;;
-        d) docking_slot=${OPTARG};;
-        c) connection_url=${OPTARG};;
-        n) num_drones=${OPTARG};;
-        m) max_missing=${OPTARG};;
+        s) state=${OPTARG}
+            INPUTARGS+="--state $state "
+        ;;
+        d) docking_slot=${OPTARG}
+            INPUTARGS+="--docking-slot $docking_slot "
+        ;;
+        c) connection_url=${OPTARG}
+            INPUTARGS+="--connection-url $connection_url "
+        ;;
+        n) frame=${OPTARG}
+            INPUTARGS+="--frame $frame "
+        ;;
+        m) max_missing=${OPTARG}
+            INPUTARGS+="--max-missing $max_missing "
+        ;;
         w) network=${OPTARG};;
         p) password=${OPTARG};;
     esac
 done
+
 # Edit /etc/wpa_supplicant/wpa_supplicant.conf
 # Replace the line starting with ssid with ssid="{network name}", where {network name} is the name of the network we will connect the raspberry pi to
 sed -i "/ssid/c\ssid=$network" /etc/wpa_supplicant/wpa_supplicant.conf
@@ -55,8 +64,8 @@ mkdir build && cd build
 cmake ..
 make
 # Edit most recent crontab
-@reboot bash -c "export PATH="$PATH:/usr/sbin" && cd ~/aviata/controls/build && source ../../rpi_tools/env_setup.sh && $sh && ../../rpi_tools/wait_for_mesh_network.sh && ./aviata_drone --id $id --state $state --docking-slot $docking_slot --num-drones $num_drones --connection-url $connection_url --max-missing $max_missing &>aviata_drone.out &"
+@reboot bash -c "export PATH="$PATH:/usr/sbin" && cd ~/aviata/controls/build && source ../../rpi_tools/env_setup.sh && $sh && ../../rpi_tools/wait_for_mesh_network.sh && ./aviata_drone $INPUTARGS &>aviata_drone.out &"
 # Reboot the Rpi
 sudo reboot
 # Concatenate and return the output file from build folder (aviata_drone.out I think)
-cat ~/aviata/controls/build/aviata_drone.out
+cat ~/aviata/controls/build/aviata_drone.out    
