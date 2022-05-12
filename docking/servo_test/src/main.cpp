de468ec70a9a0a67cfb12b42173cf4f93de99b76
chirag-singh1 <66097224+chirag-singh-mykaarma@users.noreply.github.com>
2022-05-12 10:25:44 -0700

    Fixed typo

--- docking/servo_test/src/main.cpp
+++ docking/servo_test/src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <stdlib.h>
 
-int main(int argc, char*[]argv){
+int main(int argc, char*argv[]){
     if(argc != 2){
         std::cout << "One argument required: pwm cycle for servo" << std::endl;
         return 1;
